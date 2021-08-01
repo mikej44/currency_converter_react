67b15ba4bfbefcbf03bd4521275a6c5f4f1de943
Michal Jablonski <mikej44@vp.pl>
2021-08-01 14:00:35 +0200

    init commit

---
+++ src/Form/Result/index.js
@@ -0,0 +1,12 @@
+const Result = ({ result }) => (
+    <p className="result">
+        {result !== undefined && (
+            <>
+                {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;= 
+                {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
+            </>
+        )}
+    </p>
+);
+
+export default Result;
